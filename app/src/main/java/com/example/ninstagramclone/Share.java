c1cac0fb398ef65b8d22dc332dfddcee78a138bb
nee-vir <58812268+nee-vir@users.noreply.github.com>
2020-04-04 22:03:21 +0530

    Tab Layout Element Share

--- app/src/main/java/com/example/ninstagramclone/Share.java
+++ app/src/main/java/com/example/ninstagramclone/Share.java
@@ -1,18 +1,49 @@
 package com.example.ninstagramclone;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Share extends Fragment {
+public class Share extends Fragment implements View.OnClickListener{
+    private ImageView imageView;
+    private EditText description;
+    private Button shareButton;
+    Bitmap receivedImageBitmap;
 
     public Share() {
         // Required empty public constructor
@@ -23,6 +54,101 @@ public class Share extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false);
+        View view= inflater.inflate(R.layout.fragment_share, container, false);
+        imageView=view.findViewById(R.id.image_one);
+        description=view.findViewById(R.id.textDescription);
+        shareButton=view.findViewById(R.id.btnShare);
+        imageView.setOnClickListener(this);
+        shareButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.image_one:
+                if(Build.VERSION.SDK_INT>23 && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!=
+                        PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+                }else{
+                    getChosenImage();
+                }
+                break;
+            case R.id.btnShare:
+                if(receivedImageBitmap!=null){
+                    if(description.getText().toString().equals("")){
+                        FancyToast.makeText(getContext(),"Description cannot be left empty",FancyToast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                    } else{
+                        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+                        receivedImageBitmap.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
+                        byte [] bytes=byteArrayOutputStream.toByteArray();
+                        ParseFile parseFile=new ParseFile("pic.png",bytes);
+                        ParseObject parseObject=new ParseObject("Photo");
+                        parseObject.put("picture",parseFile);
+                        parseObject.put("images_des",description.getText().toString());
+                        parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                        final ProgressDialog dialog=new ProgressDialog(getContext());
+                        dialog.setMessage("Loading...");
+                        dialog.show();
+                        parseObject.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if(e==null){
+                                    FancyToast.makeText(getContext(),"Shared Successfully",FancyToast.LENGTH_SHORT,FancyToast.SUCCESS,false).show();
+                                } else {
+                                    FancyToast.makeText(getContext(),"Something went wrong",FancyToast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                                }
+                                dialog.dismiss();
+                            }
+                        });
+                    }
+                } else{
+                    FancyToast.makeText(getContext(),"No Image Selected",FancyToast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                }
+                break;
+        }
+    }
+
+    private void getChosenImage() {
+
+//        FancyToast.makeText(getContext(),"Now we can access the images",FancyToast.LENGTH_SHORT,FancyToast.INFO,false).show();
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,2000);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==100){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                getChosenImage();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { //This part is not working
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==2000){
+            if(resultCode== Activity.RESULT_OK){
+                try{
+                    Uri selectedImage=data.getData();
+                    String[] filePathColumn={ MediaStore.Images.Media.DATA};
+                    Cursor cursor=getActivity().getContentResolver().query(selectedImage,filePathColumn,null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex= cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath=cursor.getString(columnIndex);
+                    cursor.close();
+                    receivedImageBitmap= BitmapFactory.decodeFile(picturePath);
+                    imageView.setImageBitmap(receivedImageBitmap);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/ninstagramclone/Users.java
+++ app/src/main/java/com/example/ninstagramclone/Users.java
@@ -7,12 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Users extends Fragment {
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public Users() {
         // Required empty public constructor
@@ -23,6 +36,26 @@ public class Users extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view= inflater.inflate(R.layout.fragment_users, container, false);
+        listView=view.findViewById(R.id.listView);
+        arrayList=new ArrayList();
+        arrayAdapter=new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+        ParseQuery<ParseUser> parseQuery=ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e==null){
+                    if(objects.size()>0){
+                        for(ParseUser user: objects){
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 }
